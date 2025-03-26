04e98dea7f8ca66bcbd81b20f9a2c8abcb2ff5e1
Jason Yu <jasonyu.kr@gmail.com>
2025-03-26 13:41:53 +0900

    Rewrite focus_obsidian.sh as well

--- focus_obsidian.sh
+++ focus_obsidian.sh
@@ -1,23 +1,49 @@
 #!/bin/zsh
 
-local CNT
 local APPNAME
-local APPID
-
-APPNAME="Obsidian"
-APPID="md.obsidian"
+local STATUS
+local ITEM
+local FULL_LIST
+local LIST
+local CNT
+local CURR_DESKTOP
 
 PATH=$PATH$:/opt/bin
 
-CNT=$(countwnd $APPID)
+APPNAME="Obsidian"
 
-if [ -z "$CNT" ] || [ "$CNT" = "Z" ]; then
+STATUS=$(appstatus $APPNAME)
+if [ ! "$STATUS" = "running" ]; then
   echo "App not running. Launch and activate"
   osascript -e "tell application \"$APPNAME\"" -e "launch" -e "activate" -e "end tell"
-elif [ "$CNT" = "0" ]; then
+  exit
+fi
+
+FULL_LIST=$(listwnd)
+LIST=$(echo $FULL_LIST | rg "\"$APPNAME\"")
+if [ -z "$LIST" ]; then
+  CNT="0"
+else
+  CNT=$(echo $LIST | wc -l | trim)
+fi
+if [ "$CNT" = "0" ]; then
   echo "No window found. Open new window"
   open 'obsidian://new-window'
-else
-  echo "Window found. Activate"
-  osascript -e "tell application \"$APPNAME\"" -e "activate" -e "end tell"
+  exit
+fi
+
+CURR_DESKTOP=$(echo $FULL_LIST | rg "^[0-9] true " | choose 0)
+ITEM=$(echo $LIST | grep "^${CURR_DESKTOP} " | head -n 1)
+if [ ! -z "$ITEM" ]; then
+  echo "Window found in current desktop. Focus it"
+  WINDOW_ID=$(echo $ITEM | choose 2)
+  yabai -m window --focus $WINDOW_ID
+  exit
+fi
+ITEM=$(echo $LIST | grep -v "^${CURR_DESKTOP} " | head -n 1)
+if [ ! -z "$ITEM" ]; then
+  echo "Window found in another desktop. Focus it"
+  WINDOW_ID=$(echo $ITEM | choose 2)
+  yabai -m window --focus $WINDOW_ID
+  exit
 fi
