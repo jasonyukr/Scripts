c5f0d2e42ee36b7547696462c6f9787b750e86e3
Jason Yu <jasonyu.kr@gmail.com>
2026-04-25 23:03:31 +0900

    Add move_window_to_bottom_of_stack.sh

---
+++ move_window_to_bottom_of_stack.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+PATH=$PATH:/opt/bin:/opt/homebrew/bin
+
+if [[ -x /opt/homebrew/bin/hs ]]; then
+    hs=/opt/homebrew/bin/hs
+elif [[ -x /Applications/Hammerspoon.app/Contents/Frameworks/hs/hs ]]; then
+    hs=/Applications/Hammerspoon.app/Contents/Frameworks/hs/hs
+else
+    hs=$(command -v hs)
+fi
+
+if [[ -z $hs ]]; then
+    print -u2 "Hammerspoon hs binary not found. Install Hammerspoon (for example: brew install --cask hammerspoon) and enable Accessibility permissions."
+    exit 1
+fi
+
+"$hs" -c 'local w=hs.window.focusedWindow(); if w then w:sendToBack() end'
